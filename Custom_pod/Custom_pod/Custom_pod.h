dfcfee7b20eb44afc7b37de70b84a16e809216bb
PMTIOS <>
2020-11-14 17:36:02 +0530

    Initial

---
+++ Custom_pod/Custom_pod/Custom_pod.h
@@ -0,0 +1,18 @@
+////  Custom_pod.h
+//  Custom_pod
+//
+//  Created by Naveen.K on 14/11/20.
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Custom_pod.
+FOUNDATION_EXPORT double Custom_podVersionNumber;
+
+//! Project version string for Custom_pod.
+FOUNDATION_EXPORT const unsigned char Custom_podVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Custom_pod/PublicHeader.h>
+
+
---
+++ Custom_pod/Custom_pod/SmpClass.swift
@@ -0,0 +1,17 @@
+////  SmpClass.swift
+//  Custom_pod
+//
+//  Created by Naveen.K on 14/11/20.
+//  
+//
+
+import Foundation
+
+public class SmpClass{
+    
+    private init(){}
+    
+    public static func sum(_ a:Int,_ b:Int) -> Int{
+        return a+b
+    }
+}
